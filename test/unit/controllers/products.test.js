e65116fff18d13e17be4c9753d9bc8ee8ca61177
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-23 16:21:16 -0300

    fix: corrigi erros de test

--- test/unit/controllers/products.test.js
+++ test/unit/controllers/products.test.js
@@ -1,22 +1,26 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const { ProductsController } = require('../../../controllers/products');
+const ProductsController = require('../../../controllers/products');
+const connection = require('../../../models/connection');
 
 describe('Verifica rota de produtos', () => {
   const response = {};
   const request = {};
 
   before(() => {
+    const execute = [{}];
+
+    sinon.stub(connection, 'execute').resolves(execute);
     request.body = {};
 
     response.status = sinon.stub()
       .returns(response);
-    response.send = sinon.stub()
+    response.json = sinon.stub()
       .returns();
   });
 
   it('rota volta status de 200 ao utilizar get', async () => {
-    await ProductsController.create(request, response);
+    await ProductsController.get(request, response);
 
     expect(response.status.calledWith(200)).to.be.equal(true);
   });
