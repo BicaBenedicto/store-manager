16217c0e33ebefa0dc30b49a7a22d3b3adf85ff8
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-23 21:45:50 -0300

    feat: adiciona teste de post para rota sales

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -23,7 +23,7 @@ describe('Verifica controller de sales', () => {
     ], []];
 
     sinon.stub(connection, 'execute').resolves(execute);
-    request.body = {};
+    request.body = [{ productId: 1, quantity: 10 }];
     request.params = {
       id: 1,
     };
@@ -49,6 +49,11 @@ describe('Verifica controller de sales', () => {
     await SalesController.getById(request, response);
     expect(response.status.calledWith(200)).to.be.equal(true);
   });
+
+  it('rota volta status de 201 ao utilizar post', async () => {
+    await SalesController.create(request, response);
+    expect(response.status.calledWith(201)).to.be.equal(true);
+  });
 });
 
 describe('Verifica erros de controller em sales', () => {
@@ -61,7 +66,7 @@ describe('Verifica erros de controller em sales', () => {
     next = sinon.stub().returns();
 
     sinon.stub(connection, 'execute').resolves(execute);
-    request.body = {};
+    request.body = [{ productId: '', quantity: '' }];
     request.params = {
       id: 10,
     };
@@ -77,6 +82,10 @@ describe('Verifica erros de controller em sales', () => {
     connection.execute.restore();
   });
 
+  afterEach(() => {
+    request.body = [{ productId: '', quantity: '' }];
+  });
+
   it('controller executa next de error em get procurando por id não existente', async () => {
     await SalesController.getById(request, response, next);
     expect(next.calledWith('saleNotFound')).to.be.equal(true);
@@ -85,5 +94,39 @@ describe('Verifica erros de controller em sales', () => {
   it('rota volta status de 404 ao utilizar get procurando por id não existente', async () => {
     await ErrorsMiddleware('saleNotFound', request, response);
     expect(response.status.calledWith(404)).to.be.equal(true);
+  });
+
+  it('controller executa next de error em post sem productId', async () => {
+    request.body[0].quantity = 10;
+    await SalesController.create(request, response, next);
+    expect(next.calledWith('saleIdEmpty')).to.be.equal(true);
+  });
+
+  it('rota volta status de 400 ao utilizar post sem productId', async () => {
+    await ErrorsMiddleware('saleIdEmpty', request, response);
+    expect(response.status.calledWith(400)).to.be.equal(true);
+  });
+
+  it('controller executa next de error em post sem quantity', async () => {
+    request.body[0].productId = 1;
+    await SalesController.create(request, response, next);
+    expect(next.calledWith('productQuantityEmpty')).to.be.equal(true);
+  });
+
+  it('rota volta status de 400 ao utilizar post sem quantity', async () => {
+    await ErrorsMiddleware('productQuantityEmpty', request, response);
+    expect(response.status.calledWith(400)).to.be.equal(true);
+  });
+
+  it('controller executa next de error em post com quantity baixa', async () => {
+    request.body[0].productId = 1;
+    request.body[0].quantity = -1;
+    await SalesController.create(request, response, next);
+    expect(next.calledWith('productQuantityShort')).to.be.equal(true);
+  });
+
+  it('rota volta status de 422 ao utilizar post com quantity baixa', async () => {
+    await ErrorsMiddleware('productQuantityShort', request, response);
+    expect(response.status.calledWith(422)).to.be.equal(true);
   });
 });
