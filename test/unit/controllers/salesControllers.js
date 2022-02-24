169ed2a6d382aaf1199253c63486f8d0a0c62a90
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-24 10:50:36 -0300

    feat: adiciona teste de rota put sales

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -157,3 +157,74 @@ describe('Verifica erros de controller em sales', () => {
     expect(response.status.calledWith(422)).to.be.equal(true);
   });
 });
+
+describe('Verifica controller de sales ao atualizar', () => {
+  const response = {};
+  const request = {};
+
+  before(async () => {
+    const execute = [[{
+      "saleId": 1,
+      "itemUpdated": [
+        {
+          "productId": 1,
+          "quantity": 6
+        }
+      ]
+    }], []];
+
+    sinon.stub(connection, 'execute').resolves(execute);
+    request.body = [{ productId: 1, quantity: 10 }];
+    request.params = {
+      id: 1,
+    };
+
+    response.status = sinon.stub()
+      .returns(response);
+    response.json = sinon.stub()
+      .returns();
+
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  it('rota volta status de 200 ao utilizar get', async () => {
+    await SalesController.update(request, response);
+
+    expect(response.status.calledWith(200)).to.be.equal(true);
+  });
+});
+
+describe('Verifica erros de controller de sales ao atualizar', () => {
+  const response = {};
+  const request = {};
+  let next;
+
+  before(async () => {
+    const execute = [[], []];
+
+    next = sinon.stub().returns();
+    sinon.stub(connection, 'execute').resolves(execute);
+    request.body = [{ productId: 1, quantity: 0 }];
+    request.params = {
+      id: 1,
+    };
+
+    response.status = sinon.stub()
+      .returns(response);
+    response.json = sinon.stub()
+      .returns();
+
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  it('controller executa next de error em put com quantity baixa', async () => {
+    await SalesController.update(request, response, next);
+    expect(next.calledWith('productQuantityShort')).to.be.equal(true);
+  });
+});
